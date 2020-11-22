a3687e3c858d57825036a34c04a756b8414d039b
sajith <sajith@digitalxlabs.com>
2020-11-22 08:17:46 +0530

    Added a sample for dymanic memory allocation

---
+++ pointers/pointers_09.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define endl  '\n';
+
+int main()
+{
+    // Dynamic memory allocation
+
+    int a; // goes to stack
+    int *p; // goes to stack
+    p = new int; // goes to heap with a 4 bytes
+    *p = 10; // allocate memory from heap
+    delete p; // delete memory from heap
+
+    p = new int[20]; // allocate 20 * 4 bytes from heap
+    delete[] p; // delete from the heap
+}
