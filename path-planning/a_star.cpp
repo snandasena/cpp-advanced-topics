9fd39873da78be76a4cfb7c119d2ebe983787452
sajith <sajith@digitalxlabs.com>
2021-02-11 20:44:51 +0530

    Improved A* CPP

--- path-planning/a_star.cpp
+++ path-planning/a_star.cpp
@@ -51,6 +51,35 @@ double calculateHValue(const Pair &src, const Pair &dest)
 }
 
 
+template<size_t ROW, size_t COL>
+void tracePath(const array<array<cell, COL>, ROW> &cellDetails, const Pair &dest)
+{
+    printf("\nThe path is ");
+    stack<Pair> Path;
+    int row = dest.second;
+    int col = dest.second;
+
+    Pair next_node = cellDetails[row][col].parent;
+
+    do
+    {
+        Path.push(next_node);
+        next_node = cellDetails[row][col].parent;
+        row = next_node.first;
+        col = next_node.second;
+    } while (cellDetails[row][col] != next_node);
+
+    Path.emplace(row, col);
+    while (!Path.empty())
+    {
+        Pair p = Path.top();
+        Path.pop();
+        printf("-> (%d, %d) ", p.first, p.second);
+    }
+};
+
+
+
 int main()
 {
 
