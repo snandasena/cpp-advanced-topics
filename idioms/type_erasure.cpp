02d219a6bf56294ea6884ed6c8b6cf4c4980250e
sajith <sajith@digitalxlabs.com>
2021-03-20 08:26:03 +0530

    Improved

--- idioms/type_erasure.cpp
+++ idioms/type_erasure.cpp
@@ -3,21 +3,42 @@
 //
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+namespace example
+    {
+
+            // 1. Type erasure using void* (like in C)
+            void qsort(void *base, size_t num, size_t size, int(*compar)(const void *, const void *));
+
+            int compare(const void *a, const void *b)
+            {
+                const int arg1 = *static_cast<const int *>(a);
+                const int arg2 = *static_cast<const int *>(b);
+
+                if (arg1 < arg2) return -1;
+                if (arg1 > arg2) return 1;
+                return 0;
+            }
+
+            // Drawback: not safe
+    }
 
 
 int main()
 {
-    int arr[4] = {1, 2, 3, 4};
 
-    cout << arr[0] << "\n";
-    cout << arr << "\n";
-    int *ptr = &arr[0];
+    std::vector<int> v = {3, 1, 2};
+//    std::vector<double > v = {33, 11, 22};
+    std::qsort(v.data(), v.size(), sizeof(int), &example::compare);
 
-    ptr += 1;
-    cout << *ptr << '\n';
 
-    cout << ptr;
+    for (auto &i: v)
+    {
+        std::cout << i << " ";
+    }
+    std::cout << "\n";
     return 0;
 }
