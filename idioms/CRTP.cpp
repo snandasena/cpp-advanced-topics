84f81731beed0e7838bf0cc7c155cd2011a97a7e
sajith <sajith@digitalxlabs.com>
2021-03-21 11:49:45 +0530

    Improved

---
+++ idioms/CRTP.cpp
@@ -0,0 +1,59 @@
+//
+// Created by sajith on 3/21/21.
+//
+// Curiously recurring template pattern or static polymorphism
+
+#include <iostream>
+#include <type_traits>
+
+template<typename SpecificWeapon>
+class Weapon
+{
+public:
+    void shoot() { implementation().shoot(); }
+
+private:
+    SpecificWeapon &implementation()
+    {
+        static_assert(std::is_base_of<Weapon, SpecificWeapon>::value);
+        return *static_cast<SpecificWeapon *>(this);
+    }
+};
+
+class M16 : public Weapon<M16>
+{
+
+public:
+    void shoot()
+    {
+        std::cout << "M16\n";
+    }
+};
+
+
+class AK47 : public Weapon<AK47>
+{
+
+public:
+    void shoot()
+    {
+        std::cout << "AK-47\n";
+    }
+};
+
+template<typename SpecificWeapon>
+void run(Weapon<SpecificWeapon> &weapon)
+{
+    weapon.shoot();
+}
+
+
+int main()
+{
+    M16 m16;
+    run(m16);
+
+    AK47 ak47;
+    run(ak47);
+    return 0;
+}
