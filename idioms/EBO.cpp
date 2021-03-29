e07c57f87b492271d7b108c05bd6a5f9f2249ec5
sajith <sajith@digitalxlabs.com>
2021-03-29 13:58:14 +0530

    Imprioved

--- idioms/EBO.cpp
+++ idioms/EBO.cpp
@@ -3,5 +3,51 @@
 //
 
 
-//#include <iostream>
+#include <iostream>
 
+struct X
+{
+
+};
+
+struct Y : X
+{
+
+};
+
+struct Z
+{
+    X x;
+    int a;
+};
+
+struct W
+{
+    int x;
+};
+
+struct K : W
+{
+};
+
+struct D : X
+{
+    int x;
+};
+
+struct F : W
+{
+    int a;
+};
+
+int main()
+{
+    std::cout << sizeof(X) << "\n";
+    std::cout << sizeof(Y) << "\n";
+    std::cout << sizeof(Z) << "\n";
+    std::cout << sizeof(W) << "\n";
+    std::cout << sizeof(K) << "\n";
+    std::cout << sizeof(D) << "\n";
+    std::cout << sizeof(F) << "\n";
+    return 0;
+}
