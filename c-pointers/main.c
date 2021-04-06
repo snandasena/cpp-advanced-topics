97675aaec8f2c79f392655bef272205e888b966a
sajith <sajith@digitalxlabs.com>
2021-04-06 08:16:47 +0530

    Improved

--- c-pointers/main.c
+++ c-pointers/main.c
@@ -70,12 +70,139 @@ void run4()
     }
 }
 
+void array_pointers()
+{
+    int a[5] = {10, 11, 12, 13, 14};
+    int x;
+    int *p;
+    p = a;
+    for (x = 0; x < 5; ++x)
+    {
+        printf("%p, %p\n", &a[x], p);
+        ++p;
+    }
+
+    int *pA = a;
+    for (x = 0; x < 5; ++x)
+    {
+        printf("%d\n", *(pA + x));
+    }
+}
+
+void allocate_storage()
+{
+    void *storage;
+    storage = malloc(16);
+
+    if (storage == NULL)
+    {
+        fprintf(stderr, "Allocation failed\n");
+    } else
+    {
+        puts("Memory allocated\n");
+    }
+
+    char *chars;
+    chars = (char *) malloc(sizeof(char) * 16);
+    if (chars == NULL)
+    {
+        fprintf(stderr, "Allocation failed\n");
+    } else
+    {
+        puts("Memory allocated\n");
+    }
+}
+
+void malloc_example()
+{
+    int *storage;
+    int x;
+    storage = (int *) malloc(sizeof(int) * 5);
+    if (storage == NULL)
+    {
+        fprintf(stderr, "Allocation failed\n");
+    } else
+    {
+        puts("Memory allocated\n");
+    }
+
+    for (x = 0; x < 5; ++x)
+    {
+        printf("%p, %d\n", (storage + x), *(storage + x));
+    }
+}
+
+void calloc_example()
+{
+    int *storage;
+    int x;
+
+    storage = (int *) calloc(5, sizeof(int));
+    if (storage == NULL)
+    {
+        fprintf(stderr, "Allocation failed\n");
+    } else
+    {
+        puts("Memory allocated\n");
+    }
+    for (x = 0; x < 5; ++x)
+    {
+        printf("%p, %d\n", (storage + x), *(storage + x));
+    }
+}
+
+void realloc_example()
+{
+    int *storage;
+    int x;
+    storage = (int *) malloc(sizeof(int) * 5);
+    if (storage == NULL)
+    {
+        fprintf(stderr, "Allocation failed\n");
+    } else
+    {
+        puts("Memory allocated\n");
+    }
+
+    for (x = 0; x < 5; ++x)
+    {
+        printf("%p, %d\n", (storage + x), *(storage + x));
+    }
+
+    storage = (int *) realloc(storage, sizeof(int) * 7);
+    if (storage == NULL)
+    {
+        fprintf(stderr, "Reallocation failed\n");
+    } else
+    {
+        puts("Buffer resized\n");
+    }
+
+    *(storage + 5) = 55;
+    *(storage + 6) = 66;
+
+    for (x = 0; x < 7; ++x)
+    {
+        printf("%p, %d\n", (storage + x), *(storage + x));
+    }
+
+    free(storage);
+    storage = NULL;
+    puts("Memory freed\n");
+}
+
 int main()
 {
 
 //    run1();
 //    run2();
 //    run3();
-    run4();
+//    run4();
+//    array_pointers();
+//    allocate_storage();
+//    malloc_example();
+//    calloc_example();
+    realloc_example();
+
     return 0;
 }
