b56de6b078e07a4f63c3c585438827f8de1b1dd7
sajith <sajith@digitalxlabs.com>
2021-05-25 18:40:59 +0530

    Improved

---
+++ cppnano/threads/data_races_example.cpp
@@ -0,0 +1,52 @@
+//
+// Created by sajith on 5/25/21.
+//
+
+#include <iostream>
+#include <thread>
+#include <future>
+
+class Vehicle
+{
+public:
+    //default constructor
+    Vehicle() : _id(0)
+    {
+        std::cout << "Vehicle #" << _id << " Default constructor called" << std::endl;
+    }
+
+    //initializing constructor
+    explicit Vehicle(int id) : _id(id)
+    {
+        std::cout << "Vehicle #" << _id << " Initializing constructor called" << std::endl;
+    }
+
+    // setter and getter
+    void setID(int id) { _id = id; }
+
+    [[nodiscard]] int getID() const { return _id; }
+
+private:
+    int _id;
+};
+
+int main()
+{
+
+    Vehicle v0;
+    Vehicle v1(1);
+
+    std::future<void> ftr =
+            std::async(
+                    [](Vehicle v)
+                    {
+                        std::this_thread::sleep_for(std::chrono::milliseconds(500)); // simulate work
+                        v.setID(2);
+                    }, v0);
+
+    v0.setID(3);
+    ftr.wait();
+    std::cout << "Vehicle #" << v0.getID() << std::endl;
+
+    return 0;
+}
