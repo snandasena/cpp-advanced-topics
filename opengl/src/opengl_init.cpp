d0ac1ef5655b7d064abafc1d0be643e058ad93e4
sajith <sajith@digitalxlabs.com>
2021-06-05 21:26:25 +0530

    Improved

---
+++ opengl/src/opengl_init.cpp
@@ -0,0 +1,40 @@
+//
+// Created by sajith on 6/5/21.
+//
+
+#include <GL/freeglut.h>
+#include <stdio.h>
+
+static void RenderSceneCB()
+{
+
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+
+int main(int argc, char **argv)
+{
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+
+    int width = 1200;
+    int height = 900;
+
+    glutInitWindowSize(width, height);
+
+    int x = 200;
+    int y = 100;
+
+    glutInitWindowPosition(x, y);
+    int win = glutCreateWindow("Demo");
+    printf("window id: %d\n", win);
+
+    GLclampf Red = 0.0f, Green = 0.0f, Blue = 0.0f, Alpha = 0.0f;
+    glClearColor(Red, Green, Blue, Alpha);
+
+    glutDisplayFunc(RenderSceneCB);
+
+    glutMainLoop();
+    return 0;
+}
