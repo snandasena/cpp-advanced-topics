d9f75b7d15c5ebb8f295d1d9731b0b59cb6794fe
sajith <sajith@digitalxlabs.com>
2021-06-07 22:49:27 +0530

    Improved

--- opengl/src/opengl_triangle.cpp
+++ opengl/src/opengl_triangle.cpp
@@ -11,6 +11,9 @@
 
 GLuint VBO;
 
+const char *pVSFileName = "/home/sajith/CLionProjects/advanced-topics/opengl/sharders/shader.vs";
+const char *pFSFileName = "/home/sajith/CLionProjects/advanced-topics/opengl/sharders/shader.fs";
+
 static void RenderSceneCB()
 {
     glClear(GL_COLOR_BUFFER_BIT);
@@ -28,12 +31,98 @@ static void RenderSceneCB()
     glutSwapBuffers();
 }
 
+static void AddShader(GLuint ShaderProgram, const char *pShaderText, GLenum ShaderType)
+{
+    GLuint ShaderObj = glCreateShader(ShaderType);
+
+    if (ShaderObj == 0)
+    {
+        fprintf(stderr, "Error creating shader type %d\n", ShaderType);
+        exit(0);
+    }
+
+    const GLchar *p[1];
+    p[0] = pShaderText;
+
+    GLint Lengths[1];
+    Lengths[0] = (GLint) strlen(pShaderText);
+
+    glShaderSource(ShaderObj, 1, p, Lengths);
+
+    glCompileShader(ShaderObj);
+
+    GLint success;
+    glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        GLchar InfoLog[1024];
+        glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
+        fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
+        exit(1);
+    }
+
+    glAttachShader(ShaderProgram, ShaderObj);
+}
+
+
+static void CompileShaders()
+{
+    GLuint ShaderProgram = glCreateProgram();
+
+    if (ShaderProgram == 0)
+    {
+        fprintf(stderr, "Error creating shader program\n");
+        exit(1);
+    }
+
+    std::string vs, fs;
+
+    if (!ReadFile(pVSFileName, vs))
+    {
+        exit(1);
+    };
+
+    AddShader(ShaderProgram, vs.c_str(), GL_VERTEX_SHADER);
+
+    if (!ReadFile(pFSFileName, fs))
+    {
+        exit(1);
+    };
+
+    AddShader(ShaderProgram, fs.c_str(), GL_FRAGMENT_SHADER);
+
+    GLint Success = 0;
+    GLchar ErrorLog[1024] = {0};
+
+    glLinkProgram(ShaderProgram);
+
+    glGetProgramiv(ShaderProgram, GL_LINK_STATUS, &Success);
+    if (Success == 0)
+    {
+        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
+        exit(1);
+    }
+
+    glValidateProgram(ShaderProgram);
+    glGetProgramiv(ShaderProgram, GL_VALIDATE_STATUS, &Success);
+    if (!Success)
+    {
+        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
+        exit(1);
+    }
+
+    glUseProgram(ShaderProgram);
+}
+
 static void CreateVertexBuffer()
 {
     Vector3f Vertices[3];
     Vertices[0] = Vector3f(-1.0f, -1.0f, 0.0f); // bottom left
-    Vertices[1] = Vector3f(1.0f, -1.0f, 0.0f); // bottom right
-    Vertices[2] = Vector3f(0.0f, 1.0f, 0.0f); // top
+    Vertices[1] = Vector3f(0.0f, 1.0f, 0.0f); // top
+    Vertices[2] = Vector3f(1.0f, -1.0f, 0.0f); // bottom right
 
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -55,6 +144,7 @@ int main(int argc, char **argv)
     int y = 100;
 
     glutInitWindowPosition(x, y);
+
     int win = glutCreateWindow("Demo");
     printf("window id: %d\n", win);
 
@@ -70,6 +160,8 @@ int main(int argc, char **argv)
     glClearColor(Red, Green, Blue, Alpha);
 
     CreateVertexBuffer();
+
+    CompileShaders();
 
     glutDisplayFunc(RenderSceneCB);
 
