e665c581727f4a85b0251775ac42bcc386e1d38e
sajith <sajith@sajith.com>
2021-06-30 11:23:43 +0530

    Improved

--- cppthings/src/file_io.cpp
+++ cppthings/src/file_io.cpp
@@ -2,3 +2,41 @@
 // Created by sajith on 6/29/21.
 //
 
+#include <cstdio>
+
+constexpr int maxString = 1024;
+constexpr int repeat = 5;
+
+int main()
+{
+    const char *fn = "testfile.txt";
+    const char *str = "This is a literal c-string.\n";
+
+    puts("waiting file");
+
+    // create/write the file
+    FILE *fw = fopen(fn, "w");
+    for (int i = 0; i < repeat; ++i)
+    {
+        fputs(str, fw);
+    }
+
+    fclose(fw);
+    puts("done!");
+
+    // read  the file
+    FILE *fr = fopen(fn, "r");
+    char buf[maxString];
+
+    while (fgets(buf, maxString, fr))
+    {
+        fputs(buf, stdout);
+    }
+
+    fclose(fr);
+    remove(fn);
+
+    puts("done");
+    return 0;
+}
+
