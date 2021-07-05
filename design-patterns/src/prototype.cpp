e1d817d007119bdcc6a405c2c057fd48ad9462ef
sajith <sajith@sajith.com>
2021-07-05 19:54:28 +0530

    Improved

--- design-patterns/src/prototype.cpp
+++ design-patterns/src/prototype.cpp
@@ -37,6 +37,36 @@ public:
         cout << "Call Method from " << prototype_name_ << " with field: " << prototype_field << endl;
     }
 
-    
-
 };
+
+class ConcretePrototype1 : public Prototype
+{
+private:
+    float concrete_prototype_field1_;
+
+public:
+    ConcretePrototype1(string protype_name, float concrete_prototype_field)
+            : Prototype(std::move(protype_name)), concrete_prototype_field1_(concrete_prototype_field) {}
+
+    Prototype *Clone() const override
+    {
+        return new ConcretePrototype1(*this);
+    }
+};
+
+
+class ConcretePrototype2 : public Prototype
+{
+private:
+    float concrete_prototype_field2_;
+
+public:
+    ConcretePrototype2(string prototype_name, float concrete_prototype_field)
+            : Prototype(std::move(prototype_name)), concrete_prototype_field2_(concrete_prototype_field) {}
+};
+
+
+
+
+
+
