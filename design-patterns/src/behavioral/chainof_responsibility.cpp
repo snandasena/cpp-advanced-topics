924d32184a88b067ea10f18b4d0f71b8458c4c46
sajith <sajith@sajith.com>
2021-07-09 21:56:30 +0530

    Improved

--- design-patterns/src/behavioral/chainof_responsibility.cpp
+++ design-patterns/src/behavioral/chainof_responsibility.cpp
@@ -3,12 +3,14 @@
 //
 
 #include <iostream>
+#include <regex>
 #include <string>
+#include <vector>
 
 class StringValidator
 {
 public:
-    virtual ~StringValidator() = default;
+    virtual ~StringValidator() {};
 
     virtual StringValidator *setNext(StringValidator *next) = 0;
 
@@ -36,10 +38,119 @@ public:
     {
         if (this->next)
         {
-            return this->validate(validateString);
+            return this->next->validate(validateString);
         }
 
         return "Success!";
     }
-
 };
+
+class NotEmptyValidator : public BaseValidator
+{
+
+public:
+    NotEmptyValidator() {};
+
+    std::string validate(std::string testString) override
+    {
+        puts("Checking if empty...");
+        if (testString.empty())
+        {
+            return "Please enter a value";
+        }
+
+        return BaseValidator::validate(testString);
+    }
+};
+
+class LengthValidator : public BaseValidator
+{
+    size_t _min_length;
+
+public:
+
+    LengthValidator(int minlength) : _min_length(minlength)
+    {
+
+    }
+
+    std::string validate(std::string testString) override
+    {
+        puts("Checking string length...");
+
+        if (testString.length() < _min_length)
+        {
+            return "Please enter a valid length string";
+        }
+
+        return BaseValidator::validate(testString);
+    }
+};
+
+
+class RegexValidator : public BaseValidator
+{
+    std::string _patternName;
+    std::string _regexString;
+
+public:
+
+    RegexValidator(std::string pattern, std::string regex) : _patternName(pattern), _regexString(regex)
+    {
+
+    }
+
+    std::string validate(std::string testString) override
+    {
+        puts("Checking regx match...");
+
+        if (!std::regex_match(testString, std::regex(_regexString)))
+        {
+            return "The value entered does not match the proper format";
+        }
+
+        return BaseValidator::validate(testString);
+    }
+};
+
+bool in_array(const std::string &value, const std::vector<std::string> &array)
+{
+    return std::find(array.begin(), array.end(), value) != array.end();
+}
+
+class HistoryValidator : public BaseValidator
+{
+    std::vector<std::string> _historyItems;
+
+public:
+
+    HistoryValidator(std::vector<std::string> history) : _historyItems(history) {}
+
+    std::string validate(std::string testString) override
+    {
+        puts("Checking the string has been used before...");
+
+        if (in_array(testString, _historyItems))
+        {
+            return "Please enter a new string";
+        }
+
+        return BaseValidator::validate(testString);
+    }
+};
+
+int main()
+{
+    StringValidator *stringValidator = new BaseValidator;
+
+    stringValidator
+            ->setNext(new NotEmptyValidator)
+            ->setNext(new LengthValidator(10));
+
+    std::cout << stringValidator->validate("") << "\n\n";
+    std::cout << stringValidator->validate("test sting to validate") << "\n\n";
+    std::cout << stringValidator->validate("test") << std::endl;
+
+    delete stringValidator;
+    return 0;
+}
