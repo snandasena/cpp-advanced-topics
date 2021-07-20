a1b47ef5b54f064f8a7d02a2fd3373ed2e7854c8
sajith <sajith@sajith.com>
2021-07-20 13:14:36 +0530

    Improved

--- structural_patterns/src/SOLID/openclosed_runtime_concept.cpp
+++ structural_patterns/src/SOLID/openclosed_runtime_concept.cpp
@@ -5,6 +5,7 @@
 
 #include <string>
 #include <iostream>
+#include <vector>
 
 class Dog
 {
@@ -19,3 +20,63 @@ public:
     }
 
 };
+
+
+class Cat
+{
+    std::string m_name;
+
+public:
+    Cat(std::string name) : m_name(std::move(name)) {}
+
+    void speak() const
+    {
+        std::cout << m_name << " meowing...\n";
+    }
+
+};
+
+
+class Wrapper
+{
+public:
+    template<typename T>
+    Wrapper(T &&speakable)
+    {
+
+    }
+
+private:
+
+    class Concept
+    {
+    public:
+        virtual ~Concept() = default;
+
+        virtual void speak() = 0;
+
+    };
+};
+
+
+void talk(const std::vector<Wrapper> &vec)
+{
+    for (const auto &e: vec)
+    {
+        std::cout << "Hello. " << e.speak();
+    }
+}
+
+
+int main()
+{
+
+    std::vector<Wrapper> vec;
+
+    vec.push_back(Wrapper(Dog("Buddy")));
+    vec.push_back(Wrapper(Cat("Tom")));
+
+    talk(vec);
+
+    return 0;
+}
