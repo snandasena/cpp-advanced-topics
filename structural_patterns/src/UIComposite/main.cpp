c9fcfd2a2bc1058a95f33c3925c04dd85edd0e34
snandasena <sajithgithub@gmail.com>
2021-08-01 18:11:00 +0530

    Improved

--- structural_patterns/src/UIComposite/main.cpp
+++ structural_patterns/src/UIComposite/main.cpp
@@ -2,3 +2,36 @@
 // Created by sajith on 7/29/21.
 //
 
+#include <iostream>
+#include "Frame.h"
+#include "Button.h"
+
+
+void Display(Widget *pWidget)
+{
+    pWidget->Paint();
+}
+
+
+int main()
+{
+    Frame mainWindow{};
+    Button btnOk{};
+    mainWindow.Add(&btnOk);
+    mainWindow.Paint();
+
+    Frame childWindow{};
+    Button btnFind{};
+    childWindow.Add(&btnFind);
+
+    mainWindow.Add(&childWindow);
+    mainWindow.Paint();
+
+//    std::cout << "Changing visibility\n\n";
+//    mainWindow.SetVisibility(false);
+//    mainWindow.Paint();
+
+    Display(&btnOk);
+    Display(&mainWindow);
+    return 0;
+}
