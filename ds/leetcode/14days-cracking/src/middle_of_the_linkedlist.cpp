bc3ecea1c4d4dcc69883ef5aeeb21f272046ae95
snandasena <sajithgithub@gmail.com>
2021-09-25 07:50:31 +0530

    Improved

---
+++ ds/leetcode/14days-cracking/src/middle_of_the_linkedlist.cpp
@@ -0,0 +1,35 @@
+//
+// Created by sajith on 9/24/21.
+//
+
+#include "base.h"
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *middleNode(ListNode *head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while (fast != nullptr and fast->next != nullptr)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+};
