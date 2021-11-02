18eb476a972bc564fb12066df9da996b5a778d91
snandasena <sajithgithub@gmail.com>
2021-11-02 16:27:33 +0530

    Improved

--- low_latency/src/main.cpp
+++ low_latency/src/main.cpp
@@ -16,14 +16,30 @@ public:
     {
         cout << num << " This is an interesting task 😂\n";
     }
+
+    static void staticMethod()
+    {
+        cout << "Calling from a static method\n";
+    }
 };
+
+void testMethod()
+{
+    cout << "Child thread " << this_thread::get_id() << endl;
+}
 
 
 int main()
 {
     X x;
     thread t(&X::doSomthingInterest, &x, 10);
+    cout << "Custom thread " << t.get_id() << endl;
     t.join();
 
+    thread t1(X::staticMethod);
+    t1.join();
+    testMethod();
+    cout << "Main thread " << this_thread::get_id() << endl;
+    testMethod();
     return 0;
 }
