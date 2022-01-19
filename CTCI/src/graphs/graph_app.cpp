ed8b76166deb278c4c952828a40ea5910a9cb7ba
snandasena <sajithgithub@gmail.com>
2022-01-19 22:44:33 +0530

    improved

--- CTCI/src/graphs/graph_app.cpp
+++ CTCI/src/graphs/graph_app.cpp
@@ -11,8 +11,21 @@ class Graph
     int m_V;
     vector<vector<int>> m_adj;
 
+    void DFS_Rec(int s, vector<bool> &visited)
+    {
+        visited[s] = true;
+        cout << s << '\t';
+        for (auto const &v: m_adj[s])
+        {
+            if (!visited[v])
+            {
+                DFS_Rec(v, visited);
+            }
+        }
+    }
+
 public:
-    Graph(int v) : m_V(v), m_adj(v ) {}
+    Graph(int v) : m_V(v), m_adj(v) {}
 
     void addEdge(int u, int v)
     {
@@ -43,16 +56,25 @@ public:
 
         cout<<endl;
     }
+
+    void DFS(int s)
+    {
+        vector<bool> visited(m_V, false);
+        DFS_Rec(s, visited);
+    }
 };
 
 int main()
 {
     Graph G(5);
-    G.addEdge(0,1);
-    G.addEdge(0,3);
-    G.addEdge(1,2);
-    G.addEdge(3,2);
-    G.addEdge(3,4);
-    G.BFS(0);
+    G.addEdge(0, 1);
+    G.addEdge(0, 3);
+    G.addEdge(1, 2);
+    G.addEdge(3, 2);
+    G.addEdge(3, 4);
+//    G.BFS(0);
+    G.DFS(0);
+
+
     return 0;
 }
