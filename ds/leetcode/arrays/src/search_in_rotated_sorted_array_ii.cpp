0c98873db86db8d4ce0e4da976d64f0c5f6f6e17
snandasena <sajithgithub@gmail.com>
2022-02-21 21:32:53 +0530

    Improved

--- ds/leetcode/arrays/src/search_in_rotated_sorted_array_ii.cpp
+++ ds/leetcode/arrays/src/search_in_rotated_sorted_array_ii.cpp
@@ -9,6 +9,35 @@ class Solution
 public:
     bool search(vector<int> &nums, int target)
     {
+        int end = static_cast<int > (nums.size());
+        if (end == 0) return false;
+        int start = 0;
+        --end;
+        int mid;
+        while (start <= end)
+        {
+            mid = start + (end - start) / 2;
+            if (nums[mid] == target) return true;
 
+            if (nums[start] == nums[mid])
+            {
+                ++start;
+                continue;
+            }
+
+            bool pivotArr = nums[start] <= nums[mid];
+            bool targetArr = nums[start] <= target;
+            if (pivotArr ^ targetArr)
+            {
+                if (pivotArr) start = mid + 1;
+                else end = mid - 1;
+            }
+            else
+            {
+                if (nums[mid] < target) start = mid + 1;
+                else end = mid - 1;
+            }
+        }
+        return false;
     }
 };
