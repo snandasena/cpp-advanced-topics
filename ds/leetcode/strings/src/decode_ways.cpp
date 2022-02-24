d8bdcba2558d1a3c3e05895fa519873ef356f1b8
snandasena <sajithgithub@gmail.com>
2022-02-24 21:33:32 +0530

    Improved

--- ds/leetcode/strings/src/decode_ways.cpp
+++ ds/leetcode/strings/src/decode_ways.cpp
@@ -12,20 +12,24 @@ public:
     int numDecodings(string s)
     {
         auto N = static_cast<int >(s.size());
+        vector<int> dp(N, 0);
+        dp[0] = s[0] == '0' ? 0 : 1;
         int a;
         int b;
-        int ans = 0;
-        for (int i = 0; i < N - 1; ++i)
+        for (int i = 1; i < N; ++i)
         {
             a = s[i] - '0';
-            if (a != 0) ++ans;
-            b = s[i + 1] - '0';
-
-            a = a + b * 10;
-            if (a > 0 && a< 27) ++ans;
+            b = (10 * (s[i - 1] - '0')) + a;
+            if (a >= 1)
+            {
+                dp[i] += dp[i - 1];
+            }
+            if (b >= 10 && b <= 26)
+            {
+                dp[i] += i >= 2 ? dp[i - 2] : 1;
+            }
         }
-
-        return ans;
+        return dp[N - 1];
     }
 };
 
