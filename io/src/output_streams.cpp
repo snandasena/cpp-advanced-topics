3db1a4f54fe06aa2fbbe61739577a3d0238c69f5
sajith <sajith@csi.com>
2022-06-01 19:34:42 +0530

    improved

---
+++ io/src/output_streams.cpp
@@ -0,0 +1,66 @@
+//
+// Created by sajith-csi on 6/1/2022.
+//
+
+#include "base.h"
+
+void OutputStreamTest()
+{
+    ofstream file{"temp.txt", ios::app};
+    if (!file)
+    {
+        cerr << "ERROR\n";
+        return;
+    }
+
+    file << "Added a text\n";
+    file.close();
+}
+
+void InputStreamTest()
+{
+    ifstream file{"temp.txt"};
+    if (!file)
+    {
+        cerr << "ERROR!\n";
+        return;
+    }
+
+}
+
+struct Date
+{
+    int d;
+    int m;
+    int y;
+};
+
+void OutputBinTest()
+{
+    Date dt{10, 12, 2000};
+    ofstream file{"data.dat", ios::binary};
+    file.write((char *) &dt, sizeof(dt));
+    file.close();
+}
+
+void CountTest()
+{
+    time_t tm{time(nullptr) + 10};
+    cout << "Please wait..." << endl;
+    while (time(nullptr) < tm);
+    cout << "\nAll done\n";
+}
+
+void TestOutManipulation()
+{
+    cout << "Regular " << '\033' << '[' << "Boldface" << endl;
+}
+
+int main()
+{
+//    OutputStreamTest();
+//    OutputBinTest();
+//        CountTest();
+    TestOutManipulation();
+    return 0;
+}
