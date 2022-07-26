0cf1b64f6f27dae35448022ae90c9cda784867ef
Sajith Nandasena <sajithgithub@gmail.com>
2022-07-26 20:02:37 +0530

    Improved

---
+++ interviews/src/base.h
@@ -0,0 +1,18 @@
+//
+// Created by sajith on 7/26/22.
+//
+
+#ifndef ADVANCED_TOPICS_BASE_H
+#define ADVANCED_TOPICS_BASE_H
+
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <map>
+#include <fstream>
+#include <typeinfo>
+
+
+#endif //ADVANCED_TOPICS_BASE_H
