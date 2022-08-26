63ddeb15040da1018c612b5cfd4cd3b0be224b0f
Sajith Nandasena <sajithgithub@gmail.com>
2022-08-26 17:20:13 +0530

    Improved

---
+++ cpp20/src/base.h
@@ -0,0 +1,11 @@
+//
+// Created by sajith on 8/26/22.
+//
+
+#ifndef ADVANCED_TOPICS_BASE_H
+#define ADVANCED_TOPICS_BASE_H
+
+#include <bits/stdc++.h>
+#include <concepts>
+
+#endif //ADVANCED_TOPICS_BASE_H
