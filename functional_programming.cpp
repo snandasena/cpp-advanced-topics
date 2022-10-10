c3262b4178ae73f7c0a31d9d9ad58fc9ce752ea2
Sajith Nandasena <sajithgithub@gmail.com>
2022-10-10 15:17:39 +0530

    Improved

---
+++ functional_programming.cpp
@@ -0,0 +1,18 @@
+//
+// Created by sajith on 10/7/22.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    vector<int> v{1, 2, 3, 4, 5, 6, 7, 8};
+
+    auto res = std::reduce(v.begin(), v.end(), 0, [](int a, int b)
+    { return a + b; });
+
+    cout << res << endl;
+    return 0;
+}
